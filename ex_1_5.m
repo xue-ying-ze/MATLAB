fc8e88b71ae85b360dc1819737d8ddabda67155b
xue-ying-ze <71882639+xue-ying-ze@users.noreply.github.com>
2020-10-19 22:47:48 +0800

    Delete ex_1_5.m

--- ex_1_5.m
+++
@@ -1,6 +0,0 @@
-f1 = @(x) x^3 - 2*x -2;
-[z1,st1] = gesect(f1,1,2,10^-10)
-f2 = @(x) exp(x) + x- 7;
-[z2,st2] = gesect(f2,1,2,10^-10)
-f3 = @(x) exp(x) + sin(x) -4;
-[z3,st3] = gesect(f3,1,2,10^-10)
