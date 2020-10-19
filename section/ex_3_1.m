de8e962976b7a7935f3b90a5b3930d67c70157ea
xue-ying-ze <71882639+xue-ying-ze@users.noreply.github.com>
2020-10-19 22:09:56 +0800

    第三章相关代码

---
+++ section/ex_3_1.m
@@ -0,0 +1,12 @@
+x = [0 1 2 3];
+y = [1 2 5 10];
+y0= polyinterp(x,y,(1:0.1:3));
+x0 = 1:0.1:3;
+plot(x0,x0.^2+1,x0,y0,'*');
+legend('y=x^2+1','²åÖµ½á¹û')
+
+function y0 = polyinterp(x,y,x0)
+    n = length(x);
+    c = newtdd(x,y,n);
+    y0 = nest(n-1,c,x0,x);
+end
---
+++ section/newtdd.m
@@ -0,0 +1,13 @@
+function c = newtdd(x,y,n)
+    for j = 1:n
+        v(j,1) = y(j);
+    end
+    for i = 2:n
+        for j = 1:n+1-i
+            v(j,i) = (v(j+1,i-1) - v(j,i-1))/(x(j+i-1)-x(j));
+        end
+    end
+    for i = 1:n
+        c(i) = v(1,i);
+    end
+end
